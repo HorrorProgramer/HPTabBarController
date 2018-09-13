19fa8fb5b4aacfd136c0c495d2e37536abdcac4a
PangJunJie <xwhnnd@163.com>
2018-09-13 17:14:25 +0800

    create HPTabBarController lib

---
+++ Example/Pods/Target Support Files/HPTabBarController/HPTabBarController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HPTabBarController : NSObject
+@end
+@implementation PodsDummy_HPTabBarController
+@end
---
+++ Example/Pods/Target Support Files/Pods-HPTabBarController_Example/Pods-HPTabBarController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HPTabBarController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HPTabBarController_Example
+@end
